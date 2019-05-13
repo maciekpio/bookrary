6ccfaf077d59cf8ca9831acbf7569a78cea8fa59
evrardemi <emilien.evrard@student.uclouvain.be>
2019-05-13 12:01:17 +0200

    ajout : page de connextion , data base qui marche, page gestion de compte, ajout du tableau sur la page d'acceuil, ajout de la page d'ajout de livre, bonton biliothèque marche, ajout de la fonctionaliter admin

--- Serveur.js
+++ Serveur.js
@@ -29,45 +29,75 @@ secret: "shhhhh",
   }
 }));
 
+function connect(req){
+  if(req.session.username){
+    if(req.session.admin){
+      return req.session.username+" (Admin)";
+    }else{
+      return req.session.username;
+    }
+  }else{
+    return "Se connecter";
+  }
+
+}
+
+function visible(req){
+  if(req.session.admin){
+    return "visible";
+  }else{
+    return "invisible"
+  }
+}
+
 app.get('/', (req, res) => {
-  MongoClient.connect('mongodb://localhost:27017', (err, db) => {
+  MongoClient.connect('mongodb://localhost:27017', (err, baseD) => {
     if (err) throw err;
-    //var dbo = db.db("dbi");
-    ////var finder=dbo.collection('incidents').find();
-  //  var t_array=[];
+    var db = baseD.db("Bibliothèque");
+    var colLivre=db.collection('livreCol').find();
+    var tab=[];
     //cree un tableau depuis la base de donnee
-    //finder.forEach(function(doc, err){
-      //t_array.push(doc);
-    //}, function(){
+    colLivre.forEach(function(livre, err){
+      if(!livre.reserved){
+        tab.push(livre);
+      }
+    }, function(){
       //met le nom de l'utilisateur si il existe sinon met qu'il n est pas log
-      db.close();
-      res.render('views/index.html', { username: req.session.username || "Se connecter", date: "25 aout"});
-    //});
+      baseD.close();
+      res.render('views/index.html', {array: tab, username: connect(req), date: "25 aout", Visibylity: visible(req) });
+    });
   });
 });
 
 app.get('/connexion', (req, res) => {
-  MongoClient.connect('mongodb://localhost:27017', (err, db) => {
-    if (err) throw err;
-    //var dbo = db.db("dbi");
-    ////var finder=dbo.collection('incidents').find();
-  //  var t_array=[];
-    //cree un tableau depuis la base de donnee
-    //finder.forEach(function(doc, err){
-      //t_array.push(doc);
-    //}, function(){
-      //met le nom de l'utilisateur si il existe sinon met qu'il n est pas log
-      db.close();
-      res.render('views/Connextion.html', { username: req.session.username || "Se connecter"});
-    //});
-  });
+      if(req.session.username){
+        res.render('views/GestionCompte.html', { username: connect(req) || "Se connecter"});
+      }else{
+        res.render('views/Connextion.html', { username: connect(req) || "Se connecter"});
+      }
 });
+
+app.get('/deconexion', (req, res) => {
+  req.session.username=null;
+  req.session.admin=null;
+  res.redirect('/');
+});
+
+app.get('/ajoutDeLivre', (req, res) => {
+  if(req.session.admin){
+    res.render('views/AjoutDeLivre.html', { username: connect(req) || "Se connecter"});
+  }else{
+    res.redirect('/');
+  }
+});
+
+
 
 app.post('/newutilisateur' , (req,res) => {
     MongoClient.connect('mongodb://localhost:27017',(err, baseD) =>{
       if(err) throw err;
       var db = baseD.db("Bibliothèque");
-      var newUtilisateur= {pseudo: req.body.nomUtilisateur , mdp: req.body.mdp1Utilisateur, eMail: req.body.mailUtilisateur};
+      var newUtilisateur= {pseudo: req.body.nomUtilisateur , mdp: req.body.mdp1Utilisateur, eMail: req.body.mailUtilisateur , admin: false};
 
       db.collection("utilisateurCol").findOne({pseudo: newUtilisateur.pseudo} , (err , utilisateur) => {
           if(err) throw err;
@@ -79,6 +109,7 @@ app.post('/newutilisateur' , (req,res) => {
           				if (err) throw err;
       			});
             req.session.username = newUtilisateur.pseudo;
+            req.session.admin = false;
             baseD.close();
             res.redirect('/')
           }
@@ -91,10 +122,11 @@ app.post('/dejainscrit' ,(req,res) => {
       if(err) throw err;
       var db = baseD.db("Bibliothèque");
 
-      db.collection("utilisateurCol").findOne({pseudo: req.body.pseudo} , (err , utilisateur) => {
+      db.collection("utilisateurCol").findOne({pseudo: req.body.nomCUtilisateur },(err , utilisateur) => {
           if(err) throw err;
-          if(utilisateur && req.body.mdpUtilisateur == utilisateur.mdp){
+          if(utilisateur.pseudo ==req.body.nomCUtilisateur && req.body.mdpUtilisateur == utilisateur.mdp){
             req.session.username = utilisateur.pseudo;
+            req.session.admin = utilisateur.admin;
             baseD.close();
             res.redirect('/');
           }else{
@@ -103,6 +135,48 @@ app.post('/dejainscrit' ,(req,res) => {
           }
       });
 
+    });
+});
+
+app.post('/upgrade' ,(req,res) => {
+    MongoClient.connect('mongodb://localhost:27017',(err, baseD) =>{
+      if(err) throw err;
+      var db = baseD.db("Bibliothèque");
+
+      if(req.body.upgradeToAdmin == "123456"){
+          db.collection("utilisateurCol").updateOne({pseudo: req.session.username },{$set: {admin: true}},function(err, res) {
+            if (err) throw err;
+          });
+          req.session.admin = true;
+          res.redirect('/');
+          baseD.close();
+      }else{
+        baseD.close();
+        res.redirect('/connexion');
+      }
+    });
+});
+
+app.post('/newLivre' , (req,res) => {
+    MongoClient.connect('mongodb://localhost:27017',(err, baseD) =>{
+      if(err) throw err;
+      var db = baseD.db("Bibliothèque");
+      var newLivre= {titre: req.body.titreLivre , auteur: req.body.auteurLivre, edition: req.body.editionLivre ,
+         date: req.body.dateLivre, checkboxNom: req.body.titreLivre+req.body.auteurLivre, reserved: false};
+
+      db.collection("livreCol").findOne({titre: newLivre.titre , auteur: newLivre.auteur} , (err , livre) => {
+          if(err) throw err;
+          if(livre){
+            baseD.close();
+            res.redirect('/ajoutDeLivre');
+          }else{
+            db.collection("livreCol").insertOne(newLivre, function(err, res) {
+          				if (err) throw err;
+      			});
+            baseD.close();
+            res.redirect('/');
+          }
+      });
     });
 });
 
